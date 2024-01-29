963da53ae5faa78391b6d0215ad58ee6eaa8406c
Manasseh <believemanasseh@gmail.com>
2024-01-29 11:14:46 +0100

    chore: add pom file configuration, remove unnecessary comments

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,47 +1,69 @@
+val kotlinVersion: String by project
+
 plugins {
     alias(libs.plugins.jvm)
     `java-library`
     `maven-publish`
 }
 
+group = "com.administrativektor"
+version = "0.0.1"
+
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
-
-publishing {
-    publications {
-        create<MavenPublication>("ktor-admin") {
-            from(components["java"])
-        }
-    }
-}
-
 dependencies {
-    // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Use the JUnit 5 integration.
     testImplementation(libs.junit.jupiter.engine)
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.guava)
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api(libs.commons.math3)
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
+    // Apply a specific Java toolchain to ease working on different environments.
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("ktor-admin") {
+            artifactId = "ktor-admin"
+            from(components["java"])
+            pom {
+                name = "Ktor Admin"
+                description = "An admin interface library for ktor applications."
+                url = "https://github.com/believemanasseh/ktor-admin"
+                licenses {
+                    license {
+                        name = "The 3-Clause BSD License"
+                        url = "https://opensource.org/license/bsd-3-clause/"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "believemanasseh"
+                        name = "Believe Manasseh"
+                        email = "believemanasseh@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/believemanasseh/ktor-admin.git"
+                    developerConnection = "scm:git:ssh://github.com/believemanasseh/ktor-admin.git"
+                    url = "https://github.com/believemanasseh/ktor-admin"
+                }
+            }
+        }
     }
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
