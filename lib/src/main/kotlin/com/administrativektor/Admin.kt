0e85e0df6e7e702697fee012221a0c4857948be0
Manasseh <believemanasseh@gmail.com>
2024-06-05 00:31:50 +0100

    feat: extend methods for Admin class

--- lib/src/main/kotlin/com/administrativektor/Admin.kt
+++ lib/src/main/kotlin/com/administrativektor/Admin.kt
@@ -6,11 +6,27 @@ import org.jetbrains.exposed.sql.Database
 
 
 class Admin(private val application: Application, private val database: Database, val configuration: Configuration) {
+    private val views: MutableList<View> = mutableListOf()
+
     init {
         this.application.log.info("Admin class initialisation completed.")
     }
 
+    fun returnViews(): MutableList<View> {
+        return this.views
+    }
+
     private fun retrieveTableNames(): List<String> {
         return retrieveTableNames(database)
+    }
+
+    fun addView(view: View): Boolean {
+        return this.views.add(view)
+    }
+
+    fun addViews(views: MutableList<View>) {
+        for (view in views) {
+            this.views.add(view)
+        }
     }
 }
