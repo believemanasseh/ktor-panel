f3ea2e92cd408f0baabee78dbc5faf31bc11201b
Manasseh <believemanasseh@gmail.com>
2024-06-05 00:32:55 +0100

    refactor: remove redundant semi-colon

--- lib/src/main/kotlin/com/administrativektor/Dataclasses.kt
+++ lib/src/main/kotlin/com/administrativektor/Dataclasses.kt
@@ -4,6 +4,6 @@ package com.administrativektor
  * This is a data class representing all default configurations.
  * @property setAuthentication Boolean field indicating whether to authenticate users.
  */
-data class Configuration(var setAuthentication: Boolean);
+data class Configuration(var setAuthentication: Boolean)
 
 data class View(val name: String)
