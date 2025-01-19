d449e62ef598ed0ec8b82fe28b1f01459cb864b8
Manasseh <believemanasseh@gmail.com>
2025-01-19 20:15:16 +0100

    feat: prevent double rendering of model views

--- lib/src/main/kotlin/Admin.kt
+++ lib/src/main/kotlin/Admin.kt
@@ -1,6 +1,3 @@
-package com.administrativektor
-
-import com.administrativektor.database.retrieveTableNames
 import io.ktor.server.application.*
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Database
@@ -9,36 +6,45 @@ import org.jetbrains.exposed.sql.Database
 class Admin(private val application: Application, private val database: Database, val configuration: Configuration) {
     private val modelViews: MutableList<BaseView> = mutableListOf()
     private val models: MutableList<IntIdTable> = mutableListOf()
+    private val registeredModels: MutableList<BaseView> = mutableListOf()
 
-    init {
-        this.application.log.info("Admin class initialisation completed.")
-    }
-
-    private fun returnViews(): MutableList<BaseView> {
-        return this.modelViews
-    }
-
-    private fun retrieveTableNames(): List<String> {
-        return retrieveTableNames(database)
-    }
-
-    fun addView(view: BaseView): Boolean {
+    /**
+     * Adds model view.
+     *
+     * @property view BaseView instance to be added
+     * @return Nothing
+     */
+    fun addView(view: BaseView) {
         this.models.add(view.model)
-        return this.modelViews.add(view)
+        this.modelViews.add(view)
+        this.registerRoutes()
     }
 
+    /**
+     * Adds multiple model views.
+     *
+     * @property views List containing model views
+     * @return Nothing
+     */
     fun addViews(views: MutableList<BaseView>) {
         for (view in views) {
-            this.models.add(view.model)
-            this.modelViews.add(view)
+            this.addView(view)
         }
     }
 
-    fun registerRoutes() {
+    /**
+     * Exposes database model endpoints
+     * @return Nothing
+     */
+    private fun registerRoutes() {
         for (view in this.modelViews) {
-            view.renderIndexView("index.hbs", mapOf("models" to this.models, "adminName" to configuration.adminName))
-            view.renderCreateView("create.hbs", mapOf("model" to view.model))
-            view.renderDetailsView("details.hbs", mapOf("model" to view.model))
+            if (view !in this.registeredModels) {
+                view.renderIndexView(mapOf("models" to this.models, "adminName" to this.configuration.adminName))
+                view.renderListView(mapOf("models" to this.models))
+                view.renderCreateView(mapOf("model" to view.model))
+                view.renderDetailsView(mapOf("model" to view.model))
+                view.renderUpdateView(mapOf("model" to view.model))
+            }
         }
     }
 }
--- lib/src/main/kotlin/Dataclasses.kt
+++ lib/src/main/kotlin/Dataclasses.kt
@@ -1,5 +1,8 @@
 /**
  * A data class representing all default configurations.
+ * @property url Default url of the admin interface.
+ * @property endpoint Base endpoint name for index view.
  * @property setAuthentication Boolean field indicating whether to authenticate users.
+ * @property adminName Default admin name.
  */
-data class Configuration(var setAuthentication: Boolean, var adminName: String)
+data class Configuration(val url: String, val endpoint: String, val setAuthentication: Boolean, val adminName: String)
