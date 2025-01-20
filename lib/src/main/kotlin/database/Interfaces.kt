2a93988215123ec51aaa7f41ee807dc6ab4c3f68
Manasseh <believemanasseh@gmail.com>
2025-01-20 20:31:49 +0100

    feat: extend exposed dao class methods

--- lib/src/main/kotlin/database/Interfaces.kt
+++ lib/src/main/kotlin/database/Interfaces.kt
@@ -1,12 +1,14 @@
 package database
 
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
 
 interface DatabaseAccessObjectInterface<T> {
     fun <T> findById(id: Int, table: IntIdTable, rowMapper: (ResultRow) -> T): T?
     fun <T> findAll(table: IntIdTable, rowMapper: (ResultRow) -> T): List<T?>
-    fun save(table: IntIdTable)
-    fun update(table: IntIdTable)
-    fun delete(table: IntIdTable)
+    fun save(table: IntIdTable, saveData: (UpdateBuilder<*>) -> Unit): EntityID<Int>
+    fun update(id: Int, table: IntIdTable, updateColumns: (UpdateBuilder<*>) -> Unit): Int
+    fun delete(id: Int, table: IntIdTable): Int
 }
--- lib/src/main/kotlin/database/dao/ExposedDao.kt
+++ lib/src/main/kotlin/database/dao/ExposedDao.kt
@@ -1,39 +1,51 @@
 package database.dao
 
 import database.DatabaseAccessObjectInterface
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class ExposedDao<T> : DatabaseAccessObjectInterface<T> {
     private lateinit var database: Database
 
-    fun init(database: Database) {
+    constructor(database: Database) {
         this.database = database
     }
 
     override fun <T> findById(id: Int, table: IntIdTable, rowMapper: (ResultRow) -> T): T? {
-        return transaction(database) {
+        return transaction(this.database) {
             val resultRow = table.select { table.id eq id }.singleOrNull()
             resultRow?.let { rowMapper(it) }
         }
     }
 
     override fun <T> findAll(table: IntIdTable, rowMapper: (ResultRow) -> T): List<T?> {
-        TODO("Not yet implemented")
+        return transaction(this.database) {
+            val resultRow = table.selectAll()
+            resultRow.map { rowMapper(it) }
+        }
     }
 
-    override fun update(table: IntIdTable) {
-        TODO("Not yet implemented")
+    override fun update(id: Int, table: IntIdTable, updateColumns: (UpdateBuilder<*>) -> Unit): Int {
+        return transaction(this.database) {
+            table.update({ table.id eq id }) {
+                updateColumns(it)
+            }
+        }
     }
 
-    override fun save(table: IntIdTable) {
-        TODO("Not yet implemented")
+    override fun save(table: IntIdTable, saveData: (UpdateBuilder<*>) -> Unit): EntityID<Int> {
+        return transaction(this.database) {
+            table.insertAndGetId { saveData(it) }
+        }
     }
 
-    override fun delete(table: IntIdTable) {
-        TODO("Not yet implemented")
+    override fun delete(id: Int, table: IntIdTable): Int {
+        return transaction(this.database) {
+            table.deleteWhere { table.id eq id }
+        }
     }
 }
