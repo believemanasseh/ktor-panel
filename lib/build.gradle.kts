bbdafc70533673e3935d3783ebc697d50f564815
Manasseh <believemanasseh@gmail.com>
2025-07-28 07:11:54 +0100

    build: fix ossrh url

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -101,7 +101,7 @@ publishing {
     repositories {
         maven {
             name = "OSSRH"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
             credentials {
                 val ossrhUsername: String? by project
                 val ossrhPassword: String? by project
