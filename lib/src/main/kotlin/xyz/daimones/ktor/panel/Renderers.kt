accc1af85cb56bb3d2ed3674b646306f594e7df5
Manasseh <believemanasseh@gmail.com>
2025-08-09 18:27:14 +0100

    feat: add thymeleaf implementation of TemplateRenderer

--- lib/src/main/kotlin/xyz/daimones/ktor/panel/Renderers.kt
+++ lib/src/main/kotlin/xyz/daimones/ktor/panel/Renderers.kt
@@ -2,13 +2,14 @@ package xyz.daimones.ktor.panel
 
 import io.ktor.server.freemarker.*
 import io.ktor.server.mustache.*
+import io.ktor.server.thymeleaf.*
 
 class MustacheTemplateRenderer : TemplateRenderer {
     override fun render(
         configuration: Configuration,
         view: String,
         defaultTemplate: String,
-        model: Map<String, Any?>
+        model: Map<String, Any>
     ): Any {
         val customTemplate = getCustomTemplate(view, configuration)
         return MustacheContent(customTemplate ?: defaultTemplate, model)
@@ -20,9 +21,21 @@ class FreeMarkerTemplateRenderer : TemplateRenderer {
         configuration: Configuration,
         view: String,
         defaultTemplate: String,
-        model: Map<String, Any?>
+        model: Map<String, Any>
     ): Any {
         val customTemplate = getCustomTemplate(view, configuration)
         return FreeMarkerContent(customTemplate ?: defaultTemplate, model)
+    }
+}
+
+class ThymeleafTemplateRenderer : TemplateRenderer {
+    override fun render(
+        configuration: Configuration,
+        view: String,
+        defaultTemplate: String,
+        model: Map<String, Any>
+    ): Any {
+        val customTemplate = getCustomTemplate(view, configuration)
+        return ThymeleafContent(customTemplate ?: defaultTemplate, model)
     }
 }
