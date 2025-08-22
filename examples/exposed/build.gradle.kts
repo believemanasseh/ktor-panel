baf396a9f23be6d0835ca9e8301f23f6dcdf0d08
Manasseh <believemanasseh@gmail.com>
2025-08-22 15:39:26 +0100

    chore: modify example apps

--- examples/exposed/build.gradle.kts
+++ examples/exposed/build.gradle.kts
@@ -30,6 +30,49 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.jbcrypt)
 
+<<<<<<< Updated upstream
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit5)
 }
+=======
+    testImplementation("io.ktor:ktor-server-test-host-jvm:3.0.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.1.0")
+}
+
+
+//plugins {
+//    alias(libs.plugins.jvm)
+//    alias(libs.plugins.ktor)
+//    alias(libs.plugins.kotlin.plugin.serialization) apply false
+//    alias(libs.plugins.jpa) apply false
+//    alias(libs.plugins.allopen) apply false
+//}
+//
+//group = "com.example"
+//version = "0.0.1"
+//
+//application {
+//    mainClass = "io.ktor.server.netty.EngineMain"
+//}
+//
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    implementation(project(":lib"))
+//    implementation(libs.ktor.server.core)
+//    implementation(libs.ktor.serialization.kotlinx.json)
+//    implementation(libs.ktor.server.content.negotiation)
+//    implementation(libs.exposed.core)
+//    implementation(libs.exposed.jdbc)
+//    implementation(libs.h2)
+//    implementation(libs.ktor.server.netty)
+//    implementation(libs.logback.classic)
+//    implementation(libs.ktor.server.config.yaml)
+//    implementation(libs.jbcrypt)
+//
+//    testImplementation(libs.ktor.server.test.host)
+//    testImplementation(libs.kotlin.test.junit5)
+//}
+>>>>>>> Stashed changes
