28660c8fed8142e1b58035610773a114cee99ade
Manasseh <believemanasseh@gmail.com>
2025-10-27 01:23:22 +0100

    fix: use correct annotation

--- examples/mongo/src/main/kotlin/UsersSchema.kt
+++ examples/mongo/src/main/kotlin/UsersSchema.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bson.types.ObjectId
-import xyz.daimones.ktor.panel.database.FileUpload
+import xyz.daimones.ktor.panel.database.FileUploadField
 import xyz.daimones.ktor.panel.database.serialization.LocalDateTimeSerializer
 import java.time.LocalDateTime
 
@@ -20,7 +20,7 @@ data class User(
     val password: String,
     val isActive: Boolean = false,
     val role: Role = Role.SUPER_ADMIN,
-    @FileUpload(storage = "local", path = "/uploads")
+    @FileUploadField(storage = "local", path = "/uploads")
     val image: String? = null,
     @Serializable(with = LocalDateTimeSerializer::class)
     val created: LocalDateTime = LocalDateTime.now(),
