edb3b68223d2d0695de11fb588ee3971313ea8a3
Manasseh <believemanasseh@gmail.com>
2025-11-28 11:22:40 +0100

    test: add unit test

--- lib/src/test/kotlin/ExposedTest.kt
+++ lib/src/test/kotlin/ExposedTest.kt
@@ -1,10 +1,14 @@
 import io.ktor.server.testing.*
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.mindrot.jbcrypt.BCrypt
 import xyz.daimones.ktor.panel.Admin
 import xyz.daimones.ktor.panel.Configuration
 import xyz.daimones.ktor.panel.EntityView
+import xyz.daimones.ktor.panel.database.dao.ExposedDao
 import xyz.daimones.ktor.panel.database.entities.AdminUsers
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -13,6 +17,7 @@ import kotlin.test.assertEquals
 class ExposedTest {
     private lateinit var database: Database
     private lateinit var configuration: Configuration
+    private lateinit var dao: ExposedDao<*>
 
     @BeforeTest
     fun setup() {
@@ -37,6 +42,31 @@ class ExposedTest {
             val entityView = EntityView(AdminUsers::class)
             val tableName = admin.getTableName(entityView)
             assertEquals("admin_users", tableName, "Table name should match the entity's table name")
+        }
+    }
+
+    @Test
+    fun testDaoFindById() = testApplication {
+        dao = ExposedDao(database, AdminUsers::class)
+        createUser()
+        val entity = dao.findById(1, true)
+        println("Entity: $entity")
+        (AdminUsers::class.objectInstance as Table).columns.forEach { column ->
+            @Suppress("UNCHECKED_CAST")
+            val value = (entity as Map<String, Any?>)[column.name]
+            println("Column: ${column.name}, Value: $value")
+            if (column.name == "id") {
+                assertEquals(1, value.toString().toInt(), "ID should be 1")
+            }
+        }
+    }
+
+    fun createUser() {
+        runBlocking {
+            dao.createTable()
+            val hashedPassword = BCrypt.hashpw(configuration.adminPassword, BCrypt.gensalt())
+            val entity = mapOf("username" to configuration.adminUsername, "password" to hashedPassword)
+            dao.save(entity)
         }
     }
 }
